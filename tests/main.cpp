4f4b97ac2a6bade02b99810867703538a19b0964
David Fialho <fialho.david@gmail.com>
2015-11-25 20:32:05 +0000

    added test program that build a simple network

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include "../src/Graph.h"
 
-using namespace std;
+using namespace Network;
+
+
+Graph simpleGraph() {
+	Graph graph;
+
+	graph.addEdge(0, 1);
+	graph.addEdge(0, 2);
+	graph.addEdge(1, 2);
+	graph.addEdge(1, 3);
+	graph.addEdge(2, 3);
+
+	return graph;
+}
 
 int main() {
-    cout << "Network Connectivity Test" << endl;
+
+	Graph graph = simpleGraph();
+	std::cout << graph << std::endl;
+
     return 0;
 }
