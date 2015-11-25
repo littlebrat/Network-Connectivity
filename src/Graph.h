b7421f4eea40f64dba7c52e8db01c97a99f08e1c
David Fialho <fialho.david@gmail.com>
2015-11-25 18:50:34 +0000

    moved the link class to its own file

--- src/Graph.h
+++ src/Graph.h
@@ -7,42 +7,13 @@
 #include <memory>
 
 #include "Node.h"
+#include "Link.h"
 
 namespace Network {
 
 	class Graph {
 
 	public:
-
-		class Link {
-
-		public:
-			typedef uint8_t Flow;
-
-			Link(Node* destNode, Flow maxFlow) : destNode(destNode), maxFlow(maxFlow) {}
-
-
-			inline Node *getDestNode() const {
-				return destNode;
-			}
-
-			inline void setDestNode(Node *destNode) {
-				Link::destNode = destNode;
-			}
-
-			inline Flow getMaxFlow() const {
-				return maxFlow;
-			}
-
-			inline void setMaxFlow(Flow maxFlow) {
-				Link::maxFlow = maxFlow;
-			}
-
-		private:
-			Node* destNode;  // destination node of the link
-			Flow maxFlow;   // maximum flow that can go through this link
-
-		};
 
 		typedef std::unique_ptr<Node> NodePtr;
 		// a node list stores all nodes with Negative and Positive polarity
---
+++ src/Link.h
@@ -0,0 +1,44 @@
+#ifndef LINK_H
+#define LINK_H
+
+#include <cstdint>
+
+namespace Network {
+
+	class Node;
+
+	class Link {
+
+	public:
+		// flows can only have 3 values 0, 1 and 2
+		typedef uint8_t Flow;
+
+		Link(Node* destNode, Flow maxFlow) : destNode(destNode), maxFlow(maxFlow) {}
+
+
+		inline Node* getDestNode() const {
+			return destNode;
+		}
+
+		inline void setDestNode(Node* destNode) {
+			Link::destNode = destNode;
+		}
+
+		inline Flow getMaxFlow() const {
+			return maxFlow;
+		}
+
+		inline void setMaxFlow(Flow maxFlow) {
+			Link::maxFlow = maxFlow;
+		}
+
+	private:
+		Node* destNode;  // destination node of the link
+		Flow maxFlow;   // maximum flow that can go through this link
+
+	};
+
+}
+
+
+#endif //LINK_H
