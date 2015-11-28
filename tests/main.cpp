1773ffe9ccb61e03af67e931b73e1f54fc17e413
David Fialho <fialho.david@gmail.com>
2015-11-28 19:42:32 +0000

    added another test

--- tests/main.cpp
+++ tests/main.cpp
@@ -16,10 +16,42 @@ Graph simpleGraph() {
 	return graph;
 }
 
-int main() {
+unsigned testConnectivity(Graph& graph, Node::ID srcId, Node::ID destId) {
+	unsigned connectivity = graph.getConnectivity(srcId, destId);
+	std::cout << "connectivity(" << srcId << ", " << destId << "): " << connectivity << std::endl;
+	return connectivity;
+}
+
+void test1() {
+	std::cout << "Test 1:" << std::endl;
 
 	Graph graph = simpleGraph();
 	std::cout << graph << std::endl;
 
-    return 0;
+	if(testConnectivity(graph, 0, 3) == 2)
+		std::cout << "Test 1 OK" << std::endl;
+	else
+		std::cout << "Test 1 FAILED" << std::endl;
+}
+
+void test2() {
+	std::cout << "Test 2:" << std::endl;
+
+	Graph graph;
+	graph.addEdge(0, 1);
+	graph.addEdge(0, 2);
+	graph.addEdge(1, 2);
+	graph.addEdge(1, 3);
+	std::cout << graph << std::endl;
+
+	if(testConnectivity(graph, 0, 3) == 2)
+		std::cout << "Test 2 OK" << std::endl;
+	else
+		std::cout << "Test 2 FAILED" << std::endl;
+}
+
+int main() {
+
+	test2();
+	return 0;
 }
