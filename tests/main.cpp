7524ce7c2a83cf2baed224e63c8fd2a436f207cb
David Fialho <fialho.david@gmail.com>
2015-12-01 21:51:26 +0000

    added the distribuition to the tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -39,6 +39,16 @@ void test(Graph& graph, Node::ID srcId, Node::ID destId, unsigned testId, unsign
 	if(!verbose)
 		enableOutput();
 
+	Graph::Distribuition distribuition = graph.getDistribuition();
+
+	std::cout << "Distribuition" << std::endl;
+	std::cout << distribuition.size() << std::endl;
+
+	for(size_t connectivity = 0; connectivity < distribuition.size() - 1; connectivity++) {
+		std::cout << "connectivity[" << connectivity << "]=" << distribuition[connectivity] << std::endl;
+	}
+	std::cout << "connectivity[INFINITY]=" << distribuition[distribuition.size() - 1] << std::endl;
+
 	if(connectivity == expected)
 		std::cout << "Test " << testId << " OK" << std::endl;
 	else
@@ -61,12 +71,26 @@ void test2(bool verbose = false) {
 	test(graph, 0, 3, 2, 2, verbose);
 }
 
+void test3(bool verbose = false) {
+	Graph graph;
+	graph.addEdge(1, 2);
+	graph.addEdge(1, 5);
+	graph.addEdge(1, 6);
+	graph.addEdge(2, 3);
+	graph.addEdge(3, 5);
+	graph.addEdge(3, 4);
+	graph.addEdge(4, 6);
+
+	test(graph, 1, 4, 3, 2, verbose);
+}
+
 bool verbose = true;
 
 int main() {
 
 	test1(verbose);
 	test2(verbose);
+	test3(verbose);
 
 	return 0;
 }
