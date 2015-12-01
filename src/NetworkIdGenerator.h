48fc4b7c7f0e1e8e65470b016325e367faf8a2f7
David Fialho <fialho.david@gmail.com>
2015-12-01 12:53:28 +0000

    added the network id generator

---
+++ src/NetworkIdGenerator.h
@@ -0,0 +1,30 @@
+#ifndef NETWORKIDGENERATOR_H
+#define NETWORKIDGENERATOR_H
+
+
+#include <unordered_map>
+#include "Node.h"
+
+namespace Network {
+
+	class NetworkIdGenerator {
+
+	public:
+		NetworkIdGenerator() : netidCount(0) {}
+
+		Node::ID getNetid(Node::ID id);
+		inline Node::ID netid(Node::ID id) {
+			return idMap.at(id);
+		}
+
+	private:
+		Node::ID netidCount;                          // counter to assign unique network ids in order
+		std::unordered_map<Node::ID, Node::ID> idMap; // maps node ids to network ids
+
+	};
+
+}
+
+
+
+#endif //NETWORKIDGENERATOR_H
